cb3d22027e93346fa94c5b05d33ab20959ea2f00
Nathan Okazaki <nathan.okazaki@pucpr.edu.br>
2026-05-07 12:05:05 -0300

    Refactor generation grouping and translation methods

--- Exercicio.java
+++ Exercicio.java
@@ -1,72 +1,58 @@
-//questão 1
+// questão 1
 
 public static ArrayList<Pessoa> agrupar_geracao_X(ArrayList<Pessoa> base_de_pessoas) {
-        /*
-         Retorna o subconjunto das pessoas da base que pertencem � Gera��o X,
-         de acordo com valores de ano m�nimo e m�ximo de nascimento definidos acima.
-        */
-        ArrayList<Pessoa> listaPessoas = new ArrayList<>();
-        for (int i = 0; i < base_de_pessoas.size(); i++) {
-            Pessoa pessoa = base_de_pessoas.get(i);
-            if (pessoa.nascida_entre(1965,1980)) {
-                listaPessoas.add(pessoa);
-            }
+    ArrayList<Pessoa> listaPessoas = new ArrayList<>();
+    for (int i = 0; i < base_de_pessoas.size(); i++) {
+        Pessoa pessoa = base_de_pessoas.get(i);
+        if (pessoa.nascida_entre(1965,1980)) {
+            listaPessoas.add(pessoa);
         }
-        return listaPessoas;
     }
+    return listaPessoas;
+}
 
-    public static ArrayList<Pessoa> agrupar_geracao_Y(ArrayList<Pessoa> base_de_pessoas) {
-        /*
-         Retorna o subconjunto das pessoas da base que pertencem � Gera��o Y,
-         de acordo com valores de ano m�nimo e m�ximo de nascimento definidos acima.
-        */
-        ArrayList<Pessoa> listaPessoas = new ArrayList<>();
-        for (int i = 0; i < base_de_pessoas.size(); i++) {
-            Pessoa pessoa = base_de_pessoas.get(i);
-            if (pessoa.nascida_entre(1981,1995)) {
-                listaPessoas.add(pessoa);
-            }
+public static ArrayList<Pessoa> agrupar_geracao_Y(ArrayList<Pessoa> base_de_pessoas) {
+    ArrayList<Pessoa> listaPessoas = new ArrayList<>();
+    for (int i = 0; i < base_de_pessoas.size(); i++) {
+        Pessoa pessoa = base_de_pessoas.get(i);
+        if (pessoa.nascida_entre(1981,1995)) {
+            listaPessoas.add(pessoa);
         }
-        return listaPessoas;
     }
+    return listaPessoas;
+}
 
-    public static ArrayList<Pessoa> agrupar_geracao_Z(ArrayList<Pessoa> base_de_pessoas) {
-        /*
-         Retorna o subconjunto das pessoas da base que pertencem � Gera��o Z,
-         de acordo com valores de ano m�nimo e m�ximo de nascimento definidos acima.
-        */
-        ArrayList<Pessoa> listaPessoas = new ArrayList<>();
-        for (int i = 0; i < base_de_pessoas.size(); i++) {
-            Pessoa pessoa = base_de_pessoas.get(i);
-            if (pessoa.nascida_entre(1996,2012)) {
-                listaPessoas.add(pessoa);
-            }
+public static ArrayList<Pessoa> agrupar_geracao_Z(ArrayList<Pessoa> base_de_pessoas) {
+    ArrayList<Pessoa> listaPessoas = new ArrayList<>();
+    for (int i = 0; i < base_de_pessoas.size(); i++) {
+        Pessoa pessoa = base_de_pessoas.get(i);
+        if (pessoa.nascida_entre(1996,2012)) {
+            listaPessoas.add(pessoa);
         }
-        return listaPessoas;
     }
+    return listaPessoas;
+}
 
 // questão 2
 
-ublic String traduzir_para_portugues(String termo) {
-        try {
-            int k = localizar_ingles(termo);
-            assert (k >= 0 && k < dicionario.size());
-
-            Equivalencia eq = dicionario.get(k);
-            return eq.para_portugues();
-        } catch (AssertionError erro) {
-            return "-- TERMO INGL�S DESCONHECIDO --";
-        }
+public String traduzir_para_portugues(String termo) {
+    try {
+        int k = localizar_ingles(termo);
+        assert (k >= 0 && k < dicionario.size());
+        Equivalencia eq = dicionario.get(k);
+        return eq.para_portugues();
+    } catch (AssertionError erro) {
+        return "-- TERMO INGL�S DESCONHECIDO --";
     }
+}
 
-    public String traduzir_para_ingles(String termo) {
-        try {
-            int k = localizar_portugues(termo);
-            assert (k >= 0 && k < dicionario.size());
-
-            Equivalencia eq = dicionario.get(k);
-            return eq.para_ingles();
-        } catch (AssertionError erro) {
-            return "-- TERMO PORTUGU�S DESCONHECIDO --";
-        }
+public String traduzir_para_ingles(String termo) {
+    try {
+        int k = localizar_portugues(termo);
+        assert (k >= 0 && k < dicionario.size());
+        Equivalencia eq = dicionario.get(k);
+        return eq.para_ingles();
+    } catch (AssertionError erro) {
+        return "-- TERMO PORTUGU�S DESCONHECIDO --";
     }
+}
